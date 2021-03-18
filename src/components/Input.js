7a8bfc66a028b10e96bebd0d590a16e8d68de4b4
Everton Dias <esrdias@gmail.com>
2021-03-18 18:38:57 -0300

    Criando pagina de login

---
+++ src/components/Input.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, StyleSheet, Text, TextInput } from 'react-native';
+
+function Input(props){
+	return(
+		<View style={styles.container}>
+			<Text style={styles.label}>{props.label}</Text>
+			<TextInput style={styles.input} />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		display: "flex",
+		width: "100%",
+		flexDirection: "column",
+		marginBottom: 22
+	},
+	label: {
+		color: "#F8773B",
+		fontFamily: "Poppins",
+		fontSize: 15,
+		marginLeft: 22,
+		marginBottom: 5,
+	},
+	input: {
+		height: 48,
+		display: "flex",
+		justifyContent: 'center',
+		alignItems: 'center',
+
+		borderWidth: 2.5,
+		borderRadius: 30,
+		borderColor: "#F8773B",
+
+		paddingLeft: 22,
+		paddingRight: 15,
+		color: "#2B2B2B",
+		fontFamily: "Poppins",
+		fontSize: 15,
+	}
+})
+
+export default Input;
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,67 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 
-function SignIn() {
-  return (
-    <View>
-      <Text>tela de sing in</Text>
-    </View>
-  );
+// Components
+import Input from '../../components/Input';
+
+// Assets
+import ImageFigure from "../../assets/svgs/figure2.svg";
+
+function SignIn({ navigation }) {
+	return (
+		<View style={styles.container}>
+			<View style={styles.topContainer}>
+				<Text style={styles.title}>Bem-vindo de volta!</Text>
+				<Input label="Email:" />
+				<Input label="Senha:" />
+				<TouchableOpacity onPress={() => navigation.navigate('CreateAccount')}>
+					<Text style={styles.backButton}>
+						Não tem uma conta?{" "}
+						<Text style={{ fontFamily: "Poppins Bold" }}>Cadastre.</Text>
+					</Text>
+				</TouchableOpacity>
+			</View>
+			<View style={styles.bottomContainer}>
+				<ImageFigure />
+			</View>
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		width: "100%",
+		flexDirection: "column",
+		alignItems: "center",
+		justifyContent: "space-between",
+	},
+	topContainer: {
+		width: "100%",
+		paddingHorizontal: 22,
+	},
+	title: {
+		marginTop: 28,
+		marginBottom: 25,
+		fontFamily: "Poppins Bold",
+		letterSpacing: 0,
+		fontSize: 25,
+		color: "#F8773B",
+		textAlign: "center",
+	},
+	backButton: {
+		marginTop: 12,
+		fontFamily: "Poppins",
+		fontSize: 14,
+		color: "#919191",
+		textAlign: "center",
+	},
+	bottomContainer: {
+		display: "flex",
+		justifyContent: "flex-end",
+		alignItems: "flex-end",
+		width: "100%",
+	},
+});
 
 export default SignIn;
