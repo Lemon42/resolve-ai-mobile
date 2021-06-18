94f22038f22ce1f5ebe970922e0574bff71206d9
Everton Dias <esrdias@gmail.com>
2021-06-18 11:08:53 -0300

    Adicionando botao de remover marker

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 import MapView from "react-native-maps";
 navigator.geolocation = require("@react-native-community/geolocation");
@@ -40,34 +40,71 @@ export default class Map extends Component {
 
 	render() {
 		// Verificando se o componente precisa adicionar marker
-		var addMarker = (e) => {};
-		if(this.props.setSelectLocation){
+		var addMarker = (e) => { };
+		if (this.props.setSelectLocation) {
 			addMarker = (e) => {
 				this.setState({ markers: [{ latlng: e.nativeEvent.coordinate }] });
-				console.log(e.nativeEvent.coordinate)
 				this.props.setSelectLocation(e.nativeEvent.coordinate)
 			}
 		}
 
 		return (
-			<View style={{ flex: 1, overflow: 'hidden', borderRadius: this.props.borderRadius || 0 }}>
-				<MapView
-					style={{ flex: 1 }}
-					region={this.state.region}
-					showsUserLocation
-					loadingEnabled
+			<View style={{ flex: 1 }}>
+				<View style={{ flex: 1, overflow: 'hidden', borderRadius: this.props.borderRadius || 0 }}>
+					<MapView
+						style={{ flex: 1 }}
+						region={this.state.region}
+						showsUserLocation
+						loadingEnabled
 
-					onPress={(e) => addMarker(e)}
-				>
-					{
-						this.state.markers.map((marker, index) => (
-							<MapView.Marker key={index} coordinate={marker.latlng} />
-						))
+						onPress={(e) => addMarker(e)}
+					>
+						{
+							this.state.markers.map((marker, index) => (
+								<MapView.Marker key={index} coordinate={marker.latlng} />
+							))
+						}
+					</MapView>
+				</View>
+				{
+					this.props.setSelectLocation ?
+						(
+							this.state.markers.length >= 1 ?
+								(
+									<TouchableOpacity
+										style={styles.removeMarkerButton}
+										onPress={() => {
+											this.props.setSelectLocation({});
+											this.setState({ markers: [] });
+										}}
+									>
+										<Text style={styles.removeMarkerText}>Remover localização</Text>
+									</TouchableOpacity>
+								) : (null)
+						)
+						: null
+				}
+			</View>
 
-
-					}
-				</MapView>
-			</View >
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	removeMarkerButton: {
+		marginTop: 12,
+
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		padding: 4,
+
+		borderWidth: 3,
+		borderRadius: 7,
+		borderColor: "#F53455"
+	},
+	removeMarkerText: {
+		fontFamily: "Poppins Bold",
+		color: "#F53455",
+	},
+})
