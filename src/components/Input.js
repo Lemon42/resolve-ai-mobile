82249a451f6ef9c2c72c4fb700dd6bc4d779c2ba
Everton Dias <esrdias@gmail.com>
2021-06-18 11:10:55 -0300

    Criando novo estilo de input in line

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,6 +2,65 @@ import React from "react";
 import { View, StyleSheet, TextInput, Text } from "react-native";
 
 function Input(props) {
+
+	let styles;
+
+	if (!props.inLine) {
+		styles = StyleSheet.create({
+			container: {
+				display: "flex",
+				width: "100%",
+				flexDirection: "column",
+				marginBottom: 22,
+			},
+			label: {
+				color: "#F8773B",
+				fontFamily: "Poppins",
+				fontSize: 15,
+				marginLeft: 22,
+				marginBottom: 5,
+			},
+			input: {
+				height: 48,
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+
+				borderWidth: 2.5,
+				borderRadius: 30,
+				borderColor: "#F8773B",
+
+				paddingLeft: 22,
+				paddingRight: 15,
+				color: "#2B2B2B",
+				fontFamily: "Poppins",
+				fontSize: 15,
+			},
+		});
+	} else {
+		styles = StyleSheet.create({
+			container: {
+				display: "flex",
+				width: "100%",
+				flexDirection: "column",
+				marginBottom: 22,
+			},
+			label: {
+				color: "#F8773B",
+				fontFamily: "Poppins",
+				fontSize: 15,
+			},
+			input: {
+				height: 45,
+				borderBottomWidth: 2.5,
+				borderColor: "#F8773B",
+				color: "#2B2B2B",
+				fontFamily: "Poppins",
+				fontSize: 15,
+			},
+		});
+	}
+
 	return (
 		<View style={styles.container}>
 			<Text style={styles.label}>{props.label}</Text>
@@ -13,37 +72,5 @@ function Input(props) {
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		display: "flex",
-		width: "100%",
-		flexDirection: "column",
-		marginBottom: 22,
-	},
-	label: {
-		color: "#F8773B",
-		fontFamily: "Poppins",
-		fontSize: 15,
-		marginLeft: 22,
-		marginBottom: 5,
-	},
-	input: {
-		height: 48,
-		display: "flex",
-		justifyContent: "center",
-		alignItems: "center",
-
-		borderWidth: 2.5,
-		borderRadius: 30,
-		borderColor: "#F8773B",
-
-		paddingLeft: 22,
-		paddingRight: 15,
-		color: "#2B2B2B",
-		fontFamily: "Poppins",
-		fontSize: 15,
-	},
-});
 
 export default Input;
--- src/pages/CreateProblem/index.js
+++ src/pages/CreateProblem/index.js
@@ -1,29 +1,46 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { ScrollView, View, StyleSheet, Text, TouchableOpacity } from "react-native";
 
 // Components
 import Map from "../../components/Map";
+import Input from "../../components/Input";
 
 function CreateProblem() {
 
 	const [selectLocation, setSelectLocation] = useState({});
 
 	return (
-		<View>
-			<View style={styles.mapContainer}>
-				<Map borderRadius={7} setSelectLocation={setSelectLocation} />
-			</View>
+		<ScrollView style={{ backgroundColor: '#fff', flex: 1 }}>
+			<View style={{ marginHorizontal: 6, flex: 1 }}>
+				<Text style={styles.title}>Denunciar um problema</Text>
 
-			<Text>-- {JSON.stringify(selectLocation)}</Text>
-		</View>
+				<Input label="Título:" inLine={true} />
+
+				<Text style={styles.label}>Localização:</Text>
+				<View style={styles.mapContainer}>
+					<Map borderRadius={7} setSelectLocation={setSelectLocation} />
+				</View>
+				<Text>-- {JSON.stringify(selectLocation)}</Text>
+			</View>
+		</ScrollView>
 	);
 }
 
 const styles = StyleSheet.create({
+	title: {
+		color: "#F8773B",
+		fontFamily: "Poppins Bold",
+		fontSize: 19,
+	},
+	label: {
+		color: "#F8773B",
+		fontFamily: "Poppins",
+		fontSize: 14,
+	},
 	mapContainer: {
 		height: 300,
-		margin: 4,
-	}
+		marginTop: 4,
+	},
 })
 
 export default CreateProblem;
